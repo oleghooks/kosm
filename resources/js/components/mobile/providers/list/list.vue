6d35072f659c45ef09e4193a75c532d135065086
oleghooks <destructive.group@gmail.com>
2024-02-13 17:40:15 +0300

    ref

---
+++ resources/js/components/mobile/providers/list/list.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: ['providers'],
+    methods: {
+        favorite: async function(id){
+            await post('providers.favorite', {id: id});
+            await this.getList();
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-for="(item, index) in providers" class="list_item">
+        <div class="favorite" :class="{'favorite_active': item.favorites > 0}" v-on:click="favorite(item.id)">
+        </div>
+        <div class="icon" v-on:click="info(item.id);">
+            <img :src="item.icon"></div>
+        <div class="info" v-on:click="info(item.id);">{{item.name}}</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/mobile/providers/main.vue
@@ -0,0 +1,100 @@
+<script>
+import useEventsBus from "@/EventBus.js";
+import Groups from "@/components/mobile/list/groups.vue";
+import {post} from "@/post.js";
+import List from "@/components/mobile/providers/list/list.vue";
+const {emit}=useEventsBus();
+export default {
+    components: {List, Groups},
+    props: ['cart_items'],
+    data(){
+        return{
+            providers: [
+            ],
+            currentItem: false,
+            page: 0,
+        }
+    },
+    methods: {
+        info: function(id){
+            emit('info', id);
+        },
+        getList: async function(id){
+            if(id) {
+                this.currentItem = id;
+                emit('info', id);
+            }
+            let url = "/providers.list";
+            let response = await fetch(url);
+            response = await response.json();
+            this.providers = response.response;
+        },
+        cancel: function(){
+            this.page = 0;
+            this.getList();
+        },
+        favorite: async function(id){
+            await post('providers.favorite', {id: id});
+            await this.getList();
+        }
+    },
+    mounted() {
+        this.getList();
+    }
+}
+</script>
+
+<template>
+    <groups v-if="page === 1" :cancel="cancel"></groups>
+    <div v-if="page === 0">
+        <div style="text-align: right;"><button class="button button-blue" v-on:click="page = 1">Добавить поставщиков</button></div>
+        <list :providers="providers" />
+    </div>
+</template>
+
+<style>
+.favorite{
+    width: 32px;
+    height: 32px;
+    background-image: url("/img/icons/favorites.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    margin: 10px 10px 0px 0px;
+}
+.favorite_active{
+
+    background-image: url("/img/icons/favorites-active.png");
+}
+.page_1{
+    display: none;
+}
+.active{
+    display: block;
+}
+.cart_count{
+    position: absolute;
+    background: red;
+    color: white;
+    font-weight: bold;
+    padding: 3px 7px;
+    border-radius: 19px;
+}
+
+.list_item{
+    display: flex;
+    padding: 10px;
+    border-bottom: 1px solid #bbb;
+}
+.list_item_active{
+    background: #3897f9d1;
+    color: white;
+}
+.list_item .info{
+    margin-left: 12px;
+}
+.list_item .icon img{
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+}
+</style>
